b50534fb501c5569376211b6e3376eab650a2116
oa312 <oa312@users.noreply.github.com>
2026-06-27 09:59:13 +0800

    fix: manifest start_url to /life-diary/, add service worker for PWA install

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'life-diary-v1';
+const ASSETS = [
+  '/life-diary/',
+  '/life-diary/index.html',
+  '/life-diary/manifest.json'
+];
+
+// Install
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch (network-first, fallback to cache)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
